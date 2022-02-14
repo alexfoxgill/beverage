b6f3b8f98ade7caefc22628363b79d02de8d7ab8
Alex Gill <alex.gill@depop.com>
2022-02-14 18:14:30 +0000

    Clean up action runners

--- src/turn_engine/mod.rs
+++ src/turn_engine/mod.rs
@@ -10,34 +10,32 @@ use self::{
 pub mod actions;
 pub mod effects;
 
-struct TypedActionHandler<A, S>
+struct TypedSystemRunner<In, S>
 where
-    A: Action,
-    S: System<In = A, Out = ()>,
+    S: System<In = In, Out = ()>,
 {
     system: S,
     initialized: bool,
 }
 
-impl<A, S> TypedActionHandler<A, S>
+impl<In, S> TypedSystemRunner<In, S>
 where
-    A: Action,
-    S: System<In = A, Out = ()>,
+    S: System<In = In, Out = ()>,
 {
-    pub fn new(system: S) -> TypedActionHandler<A, S> {
-        TypedActionHandler {
+    pub fn new(system: S) -> TypedSystemRunner<In, S> {
+        TypedSystemRunner {
             system,
             initialized: false,
         }
     }
 }
 
-impl<A, S> AnyActionRunner for TypedActionHandler<A, S>
+impl<A, S> AnyActionRunner for TypedSystemRunner<A, S>
 where
     A: Action,
     S: System<In = A, Out = ()>,
 {
-    fn run(&mut self, action: AnyAction, world: &mut World) {
+    fn run_action(&mut self, action: AnyAction, world: &mut World) {
         if let Ok(action) = action.0.downcast::<A>() {
             if !self.initialized {
                 self.system.initialize(world);
@@ -49,38 +47,28 @@ where
 }
 
 trait AnyActionRunner: Send + Sync {
-    fn run(&mut self, action: AnyAction, world: &mut World);
-}
-
-struct AnyActionHandler(Box<dyn AnyActionRunner>);
-
-impl AnyActionRunner for AnyActionHandler {
-    fn run(&mut self, action: AnyAction, world: &mut World) {
-        self.0.run(action, world);
-    }
+    fn run_action(&mut self, action: AnyAction, world: &mut World);
 }
 
 #[derive(Default)]
 pub struct TurnSchedules {
     effects: HashMap<TypeId, Schedule>,
-    action_handlers: HashMap<TypeId, AnyActionHandler>,
+    action_handlers: HashMap<TypeId, Box<dyn AnyActionRunner>>,
 }
 
 impl TurnSchedules {
-    pub fn register_action_system<T: Action + 'static>(
+    pub fn register_action_system<A: Action + 'static>(
         &mut self,
-        system: impl System<In = T, Out = ()>,
+        system: impl System<In = A, Out = ()>,
     ) {
-        self.action_handlers.insert(
-            TypeId::of::<T>(),
-            AnyActionHandler(Box::new(TypedActionHandler::new(system))),
-        );
+        self.action_handlers
+            .insert(TypeId::of::<A>(), Box::new(TypedSystemRunner::new(system)));
     }
 
     pub fn run_action_system(&mut self, action: AnyAction, world: &mut World) {
         let action_type = action.inner_type();
         if let Some(handler) = self.action_handlers.get_mut(&action_type) {
-            handler.run(action, world);
+            handler.run_action(action, world);
         } else {
             eprintln!("Could not find scheduler for action {:?}", action_type);
         }
